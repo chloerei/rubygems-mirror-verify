d04bc1b127f1720eef0ec689bec34fee364b4e45
Rei <chloerei@gmail.com>
2015-09-19 18:20:35 +0800

    Extract MIRROR_URL var

--- verify.sh
+++ verify.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+MIRROR_URL=https://ruby.taobao.org
+
 rm -rf vendor
 
 bundle config --delete mirror.https://rubygems.org
 bundle config --delete path
 
-bundle install --path vendor/bundle/rubygems.org
+bundle install --path vendor/bundle/source
 
-bundle config --local mirror.https://rubygems.org https://ruby.taobao.org
-bundle install --path vendor/bundle/ruby.taobao.org
+bundle config --local mirror.https://rubygems.org $MIRROR_URL
+bundle install --path vendor/bundle/mirror
 
 echo 'Comparing...'
 
-diff -r vendor/bundle/rubygems.org/ruby/2.2.0/cache/ vendor/bundle/ruby.taobao.org/ruby/2.2.0/cache/
+diff -r vendor/bundle/source/ruby/2.2.0/cache/ vendor/bundle/mirror/ruby/2.2.0/cache/
 
 echo 'Done.'
